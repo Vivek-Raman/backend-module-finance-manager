d300af7076de2e4b969365a8e9099b2a589052ae
Vivek Raman <vr.ac4bf@live.com>
2025-02-04 19:13:08 -0800

    Fix auth

--- finance-manager/src/main/java/dev/vivekraman/finance/manager/controller/TestController.java
+++ finance-manager/src/main/java/dev/vivekraman/finance/manager/controller/TestController.java
@@ -4,6 +4,8 @@ import dev.vivekraman.finance.manager.config.Constants;
 import dev.vivekraman.monolith.annotation.MonolithController;
 import dev.vivekraman.monolith.model.Response;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
@@ -13,6 +15,8 @@ import reactor.core.scheduler.Scheduler;
 public class TestController {
   private final Scheduler scheduler;
 
+
+  @PreAuthorize("hasAuthority('finance_manager')")
   @GetMapping
   public Mono<Response<Boolean>> test() {
     return Mono.just(Response.of(true))
