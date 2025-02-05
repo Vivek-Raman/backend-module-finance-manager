d6d278ab3a6769e21c1eb7294842714be5fa59fd
Vivek Raman <vr.ac4bf@live.com>
2025-02-04 18:19:05 -0800

    Configure auth

--- finance-manager-app/src/main/java/dev/vivekraman/finance/manager/app/BackendModuleFinanceManagerApplication.java
+++ finance-manager-app/src/main/java/dev/vivekraman/finance/manager/app/BackendModuleFinanceManagerApplication.java
@@ -2,8 +2,15 @@ package dev.vivekraman.finance.manager.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "dev.vivekraman.*")
+@SpringBootApplication(
+  scanBasePackages = "dev.vivekraman.*",
+  exclude = {
+    ReactiveSecurityAutoConfiguration.class,
+    ReactiveUserDetailsServiceAutoConfiguration.class,
+  })
 public class BackendModuleFinanceManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendModuleFinanceManagerApplication.class, args);
